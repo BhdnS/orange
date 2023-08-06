c6f6c27260f9f7710dd1932d5a587d4fdcebc4db
BohdanShymko <boghdan.shimko@gmail.com>
2023-08-06 13:09:23 +0300

    add: orange prod

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import './index.html';
-import './index.scss';
-import './modules/validation/formData';
-import './modules/aos/aos';
--- src/modules/validation/formData.js
+++
@@ -1,9 +0,0 @@
-import Form from './Form';
-
-const formData = new Form({
-  name: document.querySelector('#name'),
-  phone: document.querySelector('#phone'),
-});
-
-formData.nameValidation();
-formData.phoneValidation();
